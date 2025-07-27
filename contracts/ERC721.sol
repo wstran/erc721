fed23e5989a888060c9fe1f3ab23b3917ff1e436
Wilson Tran <wstran@192.168.1.14>
2025-07-27 15:03:21 +0700

    update ERC721

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -1,25 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyNFT is ERC721URIStorage, Ownable {
-    uint256 private _nextTokenId = 1;
+contract MyNFT is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
+    constructor() ERC721("MyNFT", "MTK") Ownable(msg.sender) {}
 
-    constructor() ERC721("MyNFT", "MNFT") Ownable(msg.sender) {}
-
-    function mint(address to, string memory tokenURI) external onlyOwner {
-        uint256 tokenId = _nextTokenId;
-
-        _nextTokenId++;
-
+    function safeMint(
+        address to,
+        uint256 tokenId,
+        string memory uri
+    ) public onlyOwner {
         _safeMint(to, tokenId);
-
-        _setTokenURI(tokenId, tokenURI);
+        _setTokenURI(tokenId, uri);
     }
 
-    function nextTokenId() external view returns (uint256) {
-        return _nextTokenId;
+    function tokenURI(
+        uint256 tokenId
+    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721, ERC721URIStorage) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 }
